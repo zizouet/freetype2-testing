452d6723c56db4b7afcfd0f8c452f08b7afbf07d
UnavailableCode <leopold.galhaud@gmail.com>
2025-05-13 17:40:29 +0200

    report part1

--- SUBMISSION/part1/run.w_corpus.sh.sh
+++ SUBMISSION/part1/run.w_corpus.sh.sh
@@ -2,8 +2,11 @@ PROJECT_NAME="freetype2"
 FUZZER_NAME="ftfuzzer"
 RUN_TIME=$((4 * 60 * 60))
 
+
+FUZZER_ARGS="-rss_limit_mb=2560 -fork=$(nproc) -ignore_crashes=1"
+
 echo "Building output folder"
-mkdir build/out/ftfuzzerseeds4h
+mkdir build/out/run.w_corpus
 
 echo "Building image for $PROJECT_NAME"
 python3 infra/helper.py build_image "$PROJECT_NAME"
@@ -12,9 +15,11 @@ echo "Building fuzzers for $PROJECT_NAME"
 python3 infra/helper.py build_fuzzers "$PROJECT_NAME"
 
 echo "Running $FUZZER_NAME for 4 hours"
-python3 infra/helper.py run_fuzzer "$PROJECT_NAME" "$FUZZER_NAME" --run_time="$RUN_TIME" 
+python3 infra/helper.py run_fuzzer "$PROJECT_NAME" "$FUZZER_NAME" --corpus-dir build/out/run.w_corpus --run_time="$RUN_TIME" -e FUZZER_ARGS ="$FUZZER_ARGS"
 
-echo "Creating coverage report
+echo "Creating coverage report"
+python3 infra/helper.py build_fuzzers --sanitizer coverage freetype2
+python3 infra/helper.py coverage freetyp2 --corpus-dir build/out/run.w_corpus/ --fuzz-target ftfuzzer
 
 
 
